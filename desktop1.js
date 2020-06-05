ab4db7585b76e58c5223b8b632f36988d78a4952
Stanislav Zyatyugin <zyatyugin.stas@gmail.com>
2020-06-05 16:06:20 +0300

    added list of categories

--- desktop1.js
+++ desktop1.js
@@ -4,6 +4,7 @@ window.onload = function () {
     var idInfo = document.getElementById("idInfo");
     var lastUpdate = document.getElementById("lastUpdate");
     var favSvg = document.getElementById("favSvg");
+    var form_categories=document.getElementById("form_categories");
     var frm = document.forms.form;
     var sectionRight = document.getElementById("sectionRight");
     var value_idInfo;
@@ -15,14 +16,30 @@ window.onload = function () {
         for (i = 0; i < frm.elements.length; i++) {
             var e = frm.elements[i];
             if (e.checked) {
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", "https://api.chucknorris.io/jokes/random", false);
-                xhr.send();
-                res = xhr.responseText;
-                idInfo.innerHTML = "ID: " + getData("id");
-                lastUpdate.innerHTML = "last update: " + getData("updated_at");
-                textJoke.innerHTML = getJokeValue("value");
-            }
+                if(e.name=='random'){
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", "https://api.chucknorris.io/jokes/random", false);
+                    xhr.send();
+                    res = xhr.responseText;
+                    idInfo.innerHTML = "ID: " + getData("id");
+                    lastUpdate.innerHTML = "last update: " + getData("updated_at");
+                    textJoke.innerHTML = getJokeValue("value");
+                }
+                if(e.name=='fromCategories'){
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", "https://api.chucknorris.io/jokes/categories", false);
+                    xhr.send();
+                    res = xhr.responseText;
+                    let array=res.substring(2,res.length-2).split('","');
+                    
+                    for(i=0;i<array.length;i++){
+                        var newLi_categories=document.createElement('li');
+                        form_categories.appendChild(newLi_categories);
+                        newLi_categories.innerHTML=array[i];
+                    }
+                }
+                }
+               
 
             function getData(val) {
                 var pattern = val;
@@ -40,30 +57,30 @@ window.onload = function () {
                 return res.substring(start, end);
             }
         }
-        value_idInfo = getData("id");
-        value_update = getData("updated_at");
-        value_joke = getJokeValue("value");
+    value_idInfo=getData("id");
+    value_update=getData("updated_at");
+    value_joke=getJokeValue("value");
 
     }
 
     favSvg.addEventListener('click', addtoFavourite, false);
-
+var isValid=false;
 
     function addtoFavourite(e) {
         if (!e) e = window.event;
         if (sectionRight.childNodes.length > 2) {
+            isValid=false;
             do {
                 sectionRight.removeChild(sectionRight.lastChild);
             }
             while (sectionRight.firstChild)
-
+            
         }
         window.localStorage.setItem("id", value_idInfo);
         window.localStorage.setItem("updated_at", value_update);
         window.localStorage.setItem("value", value_joke);
-        if (value_idInfo == "undefined" || value_update == "undefined" || value_joke == "undefined") {
-            e.preventDefault();
-            alert("no info to get");
+        if (value_idInfo == undefined || value_update == undefined || value_joke == undefined) {
+           return alert("no info to get");
         }
 
         function Create_div() {
@@ -72,12 +89,14 @@ window.onload = function () {
         Create_div.prototype.show_div = function (name, value_idInfo) {
             this.div.id = value_idInfo;
             this.div.className = name;
-            sectionRight.appendChild(this.div);
-            for (i = 0; i < array.length; i++) {
-                array[i].show(this.div.id);
+            if(!document.getElementById(value_idInfo)){
+                sectionRight.appendChild(this.div);
+                for (i = 0; i < array.length; i++) {
+                    array[i].show(this.div.id);
+                }
             }
+           
         }
-
         function Create_groupidInfo() {
             this.div = document.createElement("div");
             this.div.innerHTML = window.localStorage.getItem("id");
@@ -112,8 +131,25 @@ window.onload = function () {
 
 
         var r = new Create_div();
+        if(sectionRight.firstChild!=null && sectionRight.firstChild.nodeName=='#text'){
+            sectionRight.removeChild(sectionRight.firstChild);
+        }
+        if(sectionRight.childNodes.length==2){
+            r.show_div("group3", value_idInfo);
+           
+        }
+        if(sectionRight.childNodes.length==1){
+        r.show_div("group2", value_idInfo);
+        
+        }
 
+        if(!isValid){
+           
         r.show_div("group1", value_idInfo);
+        isValid=true;
+        }
+       
+        
 
 
 
